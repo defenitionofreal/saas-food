4cbc98737b2663b4b29c5b046c58e81c248fc97b
defenitionofreal <flavors@inbox.ru>
2021-12-19 19:51:33 +0300

    change model fields to chars

--- apps/company/models/social_links.py
+++ apps/company/models/social_links.py
@@ -6,12 +6,12 @@ class SocialLinks(models.Model):
     Links to other web apps
     """
     institution = models.ForeignKey("company.Institution", on_delete=models.CASCADE, related_name="social_links")
-    instagram = models.URLField()
-    vkontakte = models.URLField()
-    facebook = models.URLField()
-    youtube = models.URLField()
-    tiktok = models.URLField()
-    tripadvisor = models.URLField()
+    instagram = models.CharField(max_length=100, blank=True)  # @nickname
+    vkontakte = models.CharField(max_length=100, blank=True)
+    facebook = models.CharField(max_length=100, blank=True)
+    youtube = models.CharField(max_length=100, blank=True)
+    tiktok = models.CharField(max_length=100, blank=True)
+    tripadvisor = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
         return f"{self.institution}"
