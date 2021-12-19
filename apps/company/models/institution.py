0b5967b9946b1a038969e2ae681fc8e22786d2b8
defenitionofreal <flavors@inbox.ru>
2021-12-19 17:47:15 +0300

    add new model extra phones

--- apps/company/models/institution.py
+++ apps/company/models/institution.py
@@ -19,7 +19,8 @@ class Institution(SeoModel, AddressModel):
     logo = models.ForeignKey('media.Image', on_delete=models.CASCADE, blank=True, null=True)
     title = models.CharField(max_length=255)
     description = models.TextField(max_length=1000)
-    phone = PhoneNumberField()  # возможность добавить доп номер еще
+    phone = PhoneNumberField()
+    other_phone = models.ManyToManyField('company.ExtraPhone', blank=True, related_name="institutions")
     domain = models.CharField(max_length=255, unique=True)
     address = models.ForeignKey("location.Address", on_delete=models.SET_NULL, null=True, related_name="+")
     local_time = models.CharField(max_length=20, choices=RussianTimezone.choices, default=RussianTimezone.MOSCOW)
