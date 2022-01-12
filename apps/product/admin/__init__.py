c5cad6245a5dd922a9e04653964188d8093f8c86
defenitionofreal <flavors@inbox.ru>
2022-01-12 23:18:16 +0300

    add cat for additive

--- apps/product/admin/__init__.py
+++ apps/product/admin/__init__.py
@@ -3,3 +3,5 @@ from .category import CategoryAdmin
 from .product import ProductAdmin
 from .sticker import StickerAdmin
 from .modifiers import ModifierAdmin, ModifierPriceAdmin
+from .category_additive import CategoryAdditiveAdmin
+
