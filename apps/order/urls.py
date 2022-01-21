61f831515db86cc169063a134ef927a6f2ec8f7b
defenitionofreal <flavors@inbox.ru>
2022-01-21 17:50:20 +0300

    detal cart view

--- apps/order/urls.py
+++ apps/order/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from apps.order.api.cart import CartAPIView
+from apps.order.api.cart_detail import CartAPIView
 from apps.order.api.add_to_cart import AddToCartAPIView
 
 app_name = 'order'
-# cart достпуна по своему id
+
 urlpatterns = [
-    path('test/', CartAPIView.as_view()),
-    # добавления в корзину
-    path('customer/add-to-cart/<str:product_slug>', AddToCartAPIView.as_view()),
+    # customer добавленно в пути из-за проблем аунтентификации
+    path('customer/cart/', CartAPIView.as_view()),  # нужно ли cart определять по своему id ?
+    path('customer/add-to-cart/<str:product_slug>/', AddToCartAPIView.as_view()),
 ]
