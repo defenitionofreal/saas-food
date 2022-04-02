bfc93bde8e993d5a6a33fe9958fff5bdb09dd05e
asurovov <andrey.surovov@gmail.com>
2022-04-03 02:05:43 +0300

    Fixed base test, implement ApiTestMixin

--- tests/test_order/test_api/test_cart/test_get.py
+++ tests/test_order/test_api/test_cart/test_get.py
@@ -1,32 +1,19 @@
-# TODO: Implement factory boy, pytests for dev-development
-
-from django.test import TestCase
-from apps.company.models import Institution
-from django.contrib.auth import get_user_model
-
-from rest_framework.test import APIClient
-
-from apps.base.authentication import JWTAuthentication
-from http.cookies import SimpleCookie
 from http import HTTPStatus
+
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+from apps.company.models import Institution
+from tests.mixins import ApiTestMixin
 
 TEST_USER_PASSWORD = "AaBbCc1234"
 
 User = get_user_model()
 
 
-class GetCartTestCase(TestCase):
-
-    @classmethod
-    def setUpTestData(cls):
-        cls.user = User.objects.create(phone="89004003020")
-        cls.user.set_password(TEST_USER_PASSWORD)
-        cls.user.save()
-
-        cls.client = APIClient(defaults={"content_type": "application/json"})
-        # cls.client.cookies.load({"jwt": JWTAuthentication.generate_jwt(cls.user.id, scope="")})
-
+class GetCartTestCase(ApiTestMixin, TestCase):
     def setUp(self):
+        super().setUp()
         self.institution = Institution.objects.create(
             user=self.user,
             title="institution",
@@ -34,15 +21,20 @@ class GetCartTestCase(TestCase):
         )
 
     def test_get_as_auth(self):
-        # headers = {"jwt": JWTAuthentication.generate_jwt(self.user.id, scope="")}
+        assert self._user_cart() is None
 
-        session = self.client.session
-        session["jwt"] = JWTAuthentication.generate_jwt(self.user.id, scope="")
-        session.save()
-
-        response = self.client.get(
+        self.login_user()
+        response = self.api_client.get(
             "/api/order/{0}/customer/cart/".format(self.institution.domain),
         )
+        self.user.refresh_from_db()
 
         assert response.status_code == HTTPStatus.OK
+        assert response.data["id"] == self.user.cart_customer.id
 
+    def _user_cart(self, user=None):
+        user = user or self.user
+        try:
+            return user.cart_customer
+        except Exception:
+            return None
