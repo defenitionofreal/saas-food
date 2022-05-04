5828a5b2cb9d19bd4e5fd32ce7794c3e6af3d955
defenitionofreal <flavors@inbox.ru>
2022-05-04 19:25:59 +0300

    fix celery image path issue

--- apps/company/models/institution.py
+++ apps/company/models/institution.py
@@ -42,7 +42,7 @@ class Institution(SeoModel, AddressModel):
         return self.title
 
     def save(self, *args, **kwargs):
-        if not self.qrcode or self.qrcode is None:
+        if not self.qrcode:
             from apps.company.tasks import generate_qrcode_task
             generate_qrcode_task.delay(self.id)
         super().save(*args, **kwargs)
--- apps/company/tasks.py
+++ apps/company/tasks.py
@@ -3,26 +3,23 @@ from celery import shared_task
 # core
 from apps.base.services.salt_generator import create_salt_generator
 from apps.company.services.custom_qr_image import create_qr_titles
-from django.conf import settings
-import os
+from django.core.files.base import ContentFile
+from io import BytesIO
 
 
 @shared_task
 def generate_qrcode_task(pk):
     from apps.company.models import Institution
     institution = Institution.objects.get(pk=pk)
-
-    filepath = f'{settings.MEDIA_ROOT}/{institution.user.pk}/qrcode/'
-    if os.path.exists(filepath) is False:
-        os.makedirs(filepath)
-
-    im_path = f'{filepath}{create_salt_generator(7)}_{institution.domain}.png'
+    im_name = f"{create_salt_generator(7)}_{institution.domain}.png"
     image = create_qr_titles(
         title1='МЕНЮ',
         title2='ОПЛАТА',
         title3='БОНУСЫ',
         domain=institution.domain
     )
-    image.save(im_path)
-    institution.qrcode = im_path
-    institution.save()
+    byte_io = BytesIO()
+    image.save(byte_io, 'PNG')
+    institution.qrcode.save(im_name,
+                            ContentFile(byte_io.getvalue()),
+                            save=True)
