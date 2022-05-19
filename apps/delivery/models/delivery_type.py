f6a480ae58995552375db84cd456c568536e5526
defenitionofreal <flavors@inbox.ru>
2022-05-19 20:31:50 +0300

    upgrade delivery model

--- apps/delivery/models/delivery_type.py
+++ apps/delivery/models/delivery_type.py
@@ -1,22 +1,48 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 from apps.delivery.models.enums import (DeliveryType,
                                         SaleType,
                                         PaymentType)
 
-# TODO: institution ManyToMany
+User = get_user_model()
+
+
 class Delivery(models.Model):
     """
     Delivery model
     """
-    institution = models.ForeignKey("company.Institution",
-                                    on_delete=models.CASCADE,
-                                    related_name="delivery")
-    delivery_type = models.CharField(max_length=20, choices=DeliveryType.choices)
-    payment_type = models.CharField(max_length=20, choices=PaymentType.choices, default=PaymentType.ONLINE)
-    sale_type = models.CharField(max_length=20, choices=SaleType.choices)
-    sale_amount = models.IntegerField()
-    date_when = models.DateTimeField()
+    # (required) null because db needed default value to migrate
+    user = models.ForeignKey(User,
+                             on_delete=models.CASCADE,
+                             blank=True,
+                             null=True
+                             )
+    # organization could choose affiliates
+    institution = models.ManyToManyField("company.Institution",
+                                         related_name="delivery")
+    # create 1 of 3 delivery types
+    delivery_type = models.CharField(max_length=20,
+                                     choices=DeliveryType.choices)
+    # multiselect choice
+    payment_type = models.CharField(max_length=20,
+                                    choices=PaymentType.choices,
+                                    default=PaymentType.ONLINE)
+    # price for a specific delivery type (optional)
+    delivery_price = models.DecimalField(max_digits=10,
+                                         decimal_places=2,
+                                         blank=True,
+                                         null=True)
+    # sale for a specific delivery type (optional)
+    sale_type = models.CharField(max_length=20,
+                                 choices=SaleType.choices,
+                                 blank=True,
+                                 null=True)
+    sale_amount = models.IntegerField(blank=True,
+                                      null=True)
+    # set minimum total cart price to use delivery (optional)
+    cart_total = models.IntegerField(blank=True,
+                                     null=True)
 
     def __str__(self):
         return f"{self.institution.domain}: {self.delivery_type}"
