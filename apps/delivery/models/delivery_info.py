d49eabb13aeb2fd85d0ffdc5a03d51b07a212b3f
defenitionofreal <flavors@inbox.ru>
2022-05-20 13:42:16 +0300

    delivery info model

---
+++ apps/delivery/models/delivery_info.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from phonenumber_field.modelfields import PhoneNumberField
+
+User = get_user_model()
+
+
+class DeliveryInfo(models.Model):
+    """
+    Model for a user with customer role
+    """
+    # delivery info of a user if authorized
+    user = models.ForeignKey(User,
+                             on_delete=models.CASCADE,
+                             blank=True,
+                             null=True)
+    # user could choose a type from given
+    type = models.ForeignKey("delivery.Delivery",
+                             on_delete=models.CASCADE)
+    # required field
+    phone = PhoneNumberField()
+    # address is only required if delivery type is delivery
+    address = models.ForeignKey("location.Address",
+                                on_delete=models.SET_NULL,
+                                blank=True,
+                                null=True)
+
+    def __str__(self):
+        return f"{self.id}: {self.type} | {self.phone}"
--- apps/location/models/address.py
+++ apps/location/models/address.py
@@ -6,7 +6,9 @@ class Address(models.Model):
     region = models.ForeignKey("location.Region", on_delete=models.CASCADE)
     street = models.ForeignKey("location.Street", on_delete=models.CASCADE)
     building = models.CharField(max_length=255)
-    office = models.CharField(max_length=255)
+    office = models.CharField(max_length=255, blank=True)
+    floor = models.CharField(max_length=255, blank=True)
 
     def __str__(self):
-        return f"{self.city} {self.region} {self.street} {self.street} {self.building} {self.office}"
+        return f"{self.city} {self.region} {self.street} {self.street}\
+                 {self.building} {self.office}"
