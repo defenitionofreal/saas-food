d5220219689547c4048f1c55bfc5c77f14593d0d
defenitionofreal <flavors@inbox.ru>
2022-06-26 16:00:46 +0300

    add nginx, entrypoint, gunicorn

--- project/settings/default.py
+++ project/settings/default.py
@@ -242,8 +242,14 @@ CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
     'http://localhost:4000',
+    'http://localhost:8080',
+    'http://localhost:8000',
+    'http://localhost',
+    'http://127.0.0.1:3000',
     'http://127.0.0.1:4000',
     'http://127.0.0.1:8080',
+    'http://127.0.0.1:8000',
+    'http://127.0.0.1',
     'https://example-frontend.com',
 ]
 CORS_ALLOW_METHODS = [
