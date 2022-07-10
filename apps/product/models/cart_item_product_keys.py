0efa05d0751c4732b0fd0a70e4d4921d3b735464
User <tgt@tgt.com>
2022-07-10 12:29:14 +0300

    cart item product keys added

---
+++ apps/product/models/cart_item_product_keys.py
@@ -0,0 +1,8 @@
+cart_item_prod_id = 'id'
+cart_item_prod_title = 'title'
+cart_item_prod_category = 'category'
+cart_item_prod_slug = 'slug'
+cart_item_prod_price = 'price'
+cart_item_prod_additives = 'additives'
+cart_item_prod_modifiers = 'modifiers'
+
