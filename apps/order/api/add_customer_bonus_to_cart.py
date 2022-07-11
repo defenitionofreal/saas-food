cf7c9f0c0b2eed184a9e885be53c09fcf8050a9e
User <tgt@tgt.com>
2022-07-11 20:52:30 +0300

    refactored

--- apps/order/api/add_customer_bonus_to_cart.py
+++ apps/order/api/add_customer_bonus_to_cart.py
@@ -6,13 +6,11 @@ from rest_framework.permissions import IsAuthenticated
 from apps.company.models import Institution
 from apps.order.models import Cart, Bonus, UserBonus
 
-from decimal import Decimal
-
 
 class AddBonusAPIView(APIView):
     """
     Customer can write off his bonuses
-    from a total cart cost or total cost after promocode
+    from a total cart cost or total cost after promo code
     """
     permission_classes = [IsAuthenticated]
 
@@ -23,44 +21,36 @@ class AddBonusAPIView(APIView):
         cart = Cart.objects.get(institution=institution, customer=user)
         bonus_input = round(int(self.request.query_params.get("bonus")))
         try:
-            if cart.customer_bonus is None:
-                if bonus.is_active is True:
-                    user_bonus = UserBonus.objects.filter(
-                        institution=institution, user=user).first()
-                    if user_bonus:
-
-                        if bonus_input > user_bonus.bonus:
-                            return Response({"detail": "Not enough bonuses."})
-
-                        if cart.promo_code is not None:
-                            if bonus.is_promo_code is True:
-                                total_cart = cart.get_total_cart_after_sale
-                                sale = round((bonus.write_off / Decimal(
-                                    '100')) * total_cart)
-                            else:
-                                return Response({
-                                                    "detail": "Use bonuses with promocode is not allowed."})
-                        else:
-                            total_cart = cart.get_total_cart
-                            sale = round((bonus.write_off / Decimal(
-                                '100')) * total_cart)
-
-                        if bonus_input > sale:
-                            return Response({
-                                                "detail": f"Write off no more than {bonus.write_off}% of total price. ({sale} bonuses)"})
-                        cart.customer_bonus = bonus_input
-                        cart.save()
-                        user_bonus.bonus -= bonus_input
-                        user_bonus.save()
-                        return Response({
-                                            "detail": f"{bonus_input} bonuses have been successfully redeemed"})
-                    else:
-                        return Response(
-                            {"detail": "You dont have any bonuses yet."})
-                else:
-                    return Response(
-                        {"detail": "Loyalty program is not active"})
-            else:
+            if cart.customer_bonus is not None:
                 return Response({"detail": "Bonuses already applied."})
+
+            if bonus.is_active is False:
+                return Response({"detail": "Loyalty program is not active"})
+
+            user_bonus:UserBonus = UserBonus.objects.filter(institution=institution, user=user).first()
+            if not user_bonus:
+                return Response({"detail": "You dont have any bonuses yet."})
+
+            user_bonus_count = user_bonus.bonus
+            if bonus_input > user_bonus_count:
+                return Response({"detail": "Not enough bonuses."})
+
+            is_used_bonus_with_promo_code = cart.promo_code is not None and bonus.is_promo_code is False
+            if is_used_bonus_with_promo_code:
+                return Response({"detail": "Use bonuses with promocode is not allowed."})
+
+            total_cart = cart.get_total_cart_after_sale
+            bonus_write_off = bonus.write_off
+            sale = bonus.get_write_off_absolute_amount(total_cart)
+
+            if bonus_input > sale:
+                r = {"detail": f"Write off no more than {bonus_write_off}% of total price. ({sale} bonuses)"}
+                return Response(r)
+
+            cart.set_customer_bonus_amount(bonus_input)
+            user_bonus.write_off_bonus_amount(bonus_input)
+
+            return Response({"detail": f"{bonus_input} bonuses have been successfully redeemed"})
+
         except Exception as e:
             return Response({"detail": f"{e}"})
