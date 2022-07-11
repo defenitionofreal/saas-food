fd1fdea0407b0293745c822cd8969933bb8c79e3
User <tgt@tgt.com>
2022-07-11 17:52:03 +0300

    refactoring api

---
+++ apps/order/api/cart_from_request.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from apps.company.models import Institution
+from apps.order.models import Cart
+
+
+def get_cart_from_request(request, domain):
+    institution = Institution.objects.get(domain=domain)
+    user = request.user
+    session = request.session
+
+    if user.is_authenticated:
+        return Cart.objects.get(institution=institution, customer=user)
+    else:
+        if settings.CART_SESSION_ID in session:
+            return Cart.objects.get(institution=institution,
+                                    session_id=session[settings.CART_SESSION_ID])
--- apps/order/api/remove_from_cart.py
+++ apps/order/api/remove_from_cart.py
@@ -1,13 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.generics import get_object_or_404
-
-from apps.product.models import Product
-from apps.company.models import Institution
-from apps.order.models import Cart
-
-from django.conf import settings
-from django.db.models import F
+from apps.order.api.cart_from_request import get_cart_from_request
+from apps.order.serializers import CartSerializer
 
 
 class RemoveFromCartAPIView(APIView):
@@ -19,33 +13,12 @@ class RemoveFromCartAPIView(APIView):
     """
 
     def post(self, request, domain, product_slug):
+        cart = get_cart_from_request(request, domain)
 
-        institution = Institution.objects.get(domain=domain)
-        product = get_object_or_404(Product, slug=product_slug)
-        user = self.request.user
-        session = self.request.session
-
-        if user.is_authenticated:
-            cart = Cart.objects.filter(institution=institution, customer=user)
-        else:
-            if not settings.CART_SESSION_ID in session:
-                return Response(
-                    {"detail": "Cart does not exist. (session cart)"})
-            cart = Cart.objects.filter(institution=institution,
-                                       session_id=session[settings.CART_SESSION_ID])
-
-        if cart.exists():
-            cart = cart[0]
-            if cart.items.filter(product__slug=product.slug).exists():
-                cart_item = cart.items.get(product__slug=product.slug,
-                                           cart=cart)
-                if cart_item.quantity > 1:
-                    cart_item.quantity = F("quantity") - 1
-                    cart_item.save(update_fields=("quantity",))
-                else:
-                    cart.items.remove(cart_item)
-                return Response({"detail": "Product quantity updated"})
-            else:
-                return Response({"detail": "This product not in a cart"})
-        else:
+        if not cart:
             return Response({"detail": "Don't have a cart"})
+
+        cart.remove_product_by_slug(product_slug)
+        serializer = CartSerializer(cart, context={"request": request})
+
+        return Response(serializer.data)
