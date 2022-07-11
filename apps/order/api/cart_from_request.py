248e029b4229792309a0d20584027f6cf8f94d8b
User <tgt@tgt.com>
2022-07-11 18:40:22 +0300

    new method

--- apps/order/api/cart_from_request.py
+++ apps/order/api/cart_from_request.py
@@ -14,3 +14,14 @@ def get_cart_from_request(request, domain):
         if settings.CART_SESSION_ID in session:
             return Cart.objects.get(institution=institution,
                                     session_id=session[settings.CART_SESSION_ID])
+
+def get_or_create_cart_from_request(request, domain):
+    institution = Institution.objects.get(domain=domain)
+    user = request.user
+    session = request.session
+
+    if user.is_authenticated:
+        return Cart.objects.get_or_create(institution=institution, customer=user,
+                                          session_id=session[settings.CART_SESSION_ID])
+    else:
+        return Cart.objects.get_or_create(institution=institution, session_id=session[settings.CART_SESSION_ID])
