0ba3885cf0bdde7d4f96fd700f6c646a3864ef6a
User <tgt@tgt.com>
2022-07-11 19:36:37 +0300

    refactoring

--- apps/order/api/add_promocode_to_cart.py
+++ apps/order/api/add_promocode_to_cart.py
@@ -4,30 +4,24 @@ from rest_framework.generics import get_object_or_404
 from rest_framework import status
 
 from apps.company.models import Institution
-from apps.order.models import Cart, PromoCode, PromoCodeUser, Bonus
-
-from django.conf import settings
-import datetime
+from apps.delivery.models.enums import SaleType
+from apps.order.api.cart_from_request import get_cart_from_request
+from apps.order.models import PromoCode, PromoCodeUser, Bonus
 
 
 class AddPromoCodeAPIView(APIView):
     """ Add coupon to cart """
 
     def post(self, request, domain, coupon):
-        today = datetime.datetime.now().date()
         user = self.request.user
         code = coupon
-        coupon = get_object_or_404(PromoCode, code=code)
+        coupon: PromoCode = get_object_or_404(PromoCode, code=code)
         institution = Institution.objects.get(domain=domain)
-        session = self.request.session
 
-        if user.is_authenticated:
-            cart = Cart.objects.get(institution=institution, customer=user)
-        else:
-            if settings.CART_SESSION_ID in session:
-                cart = Cart.objects.get(institution=institution,
-                                        session_id=session[
-                                            settings.CART_SESSION_ID])
+        cart = get_cart_from_request(request, domain)
+        total_cart_price = cart.get_total_cart
+        coupon_sale = coupon.sale
+        coupon_cart_total = coupon.cart_total
 
         try:
             if cart.promo_code is None:
@@ -39,27 +33,25 @@ class AddPromoCodeAPIView(APIView):
                                         status=status.HTTP_400_BAD_REQUEST)
 
                 if coupon.is_active is True:
-                    if coupon.code_type == 'absolute' and (cart.get_total_cart - coupon.sale) <= 0:
-                        return Response({"detail": f"Sale is bigger: {cart.get_total_cart}"},
+                    if coupon.code_type == SaleType.ABSOLUTE and total_cart_price <= coupon_sale:
+                        return Response({"detail": f"Sale is bigger: {total_cart_price}"},
                                         status=status.HTTP_400_BAD_REQUEST)
-                    if coupon.code_type == 'percent' and coupon.sale > 100:
+
+                    if coupon.code_type == SaleType.PERCENT and coupon_sale > 100:
                         return Response({"detail": "Sale is bigger 100%"},
                                         status=status.HTTP_400_BAD_REQUEST)
 
-                    if coupon.cart_total is not None:
-                        if cart.get_total_cart < coupon.cart_total:
-                            return Response({"detail": f"Total cart price have to be more {coupon.cart_total}"},
-                                            status=status.HTTP_400_BAD_REQUEST)
+                    if not coupon.can_be_used_by_cart_total(total_cart_price):
+                        return Response({"detail": f"Total cart price have to be more {coupon_cart_total}"},
+                                        status=status.HTTP_400_BAD_REQUEST)
 
-                    if coupon.date_start is not None:
-                        if today < coupon.date_start:
-                            return Response({"detail": f"Code period not started yet"},
-                                            status=status.HTTP_400_BAD_REQUEST)
+                    if not coupon.can_be_used_today_by_start_date:
+                        return Response({"detail": f"Code period not started yet"},
+                                        status=status.HTTP_400_BAD_REQUEST)
 
-                    if coupon.date_finish is not None:
-                        if today >= coupon.date_finish:
-                            return Response({"detail": f"Code period expired"},
-                                            status=status.HTTP_400_BAD_REQUEST)
+                    if not coupon.can_be_used_today_by_finish_date:
+                        return Response({"detail": f"Code period expired"},
+                                        status=status.HTTP_400_BAD_REQUEST)
 
                     if coupon.categories.all():
                         x = set(cart.items.values_list('product__category', flat=True))
@@ -75,28 +67,26 @@ class AddPromoCodeAPIView(APIView):
                         if not x.intersection(y):
                             return Response(
                                 {"detail": "No products tied with coupon."},
-                            status=status.HTTP_400_BAD_REQUEST)
+                                status=status.HTTP_400_BAD_REQUEST)
 
-                    if coupon.code_use is not None:
-                        if coupon.num_uses >= coupon.code_use:
-                            return Response({"detail": "Max level exceeded for coupon."},
-                                            status=status.HTTP_400_BAD_REQUEST)
-                        coupon.num_uses += 1
-                        coupon.save()
+                    if not coupon.has_num_uses_left:
+                        return Response({"detail": "Max level exceeded for coupon."},
+                                        status=status.HTTP_400_BAD_REQUEST)
+
+                    coupon.increase_num_uses()
 
                     if user.is_authenticated:
-                        coupon_per_user, created = PromoCodeUser.objects.get_or_create(
-                            code=coupon, user=user)
-                        if coupon.code_use_by_user is not None:
-                            if coupon_per_user.num_uses >= coupon.code_use_by_user:
-                                return Response({
-                                    "detail": "User's max level exceeded for coupon."},
-                                status=status.HTTP_400_BAD_REQUEST)
-                        coupon_per_user.num_uses += 1
-                        coupon_per_user.save()
+                        coupon_per_user, created = PromoCodeUser.objects.get_or_create(code=coupon, user=user)
+                        coupon_per_user: PromoCodeUser = coupon_per_user
+                        if not coupon_per_user.can_use_this_code:
+                            return Response({"detail": "User's max level exceeded for coupon."},
+                                            status=status.HTTP_400_BAD_REQUEST)
+
+                        coupon_per_user.increase_num_uses()
 
                     cart.promo_code = coupon
                     cart.save()
+
                     return Response({"detail": "Code successfully added."},
                                     status=status.HTTP_200_OK)
                 else:
