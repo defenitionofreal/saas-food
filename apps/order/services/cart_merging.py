c3d55aeb5c07c3d63cc6cd65682375194500f2b3
User <tgt@tgt.com>
2022-07-12 20:29:28 +0300

    fix import

--- apps/order/services/cart_merging.py
+++ apps/order/services/cart_merging.py
@@ -1,8 +1,6 @@
-from apps.order.models import Cart
-
-
-def merge_two_carts(cart_to: Cart, cart_from: Cart):
+def merge_two_carts(cart_to, cart_from):
     """ Combines carts together. cart_from is not changed """
 
+    from apps.order.models import Cart
     if not isinstance(cart_to, Cart) or not isinstance(cart_from, Cart):
         return
