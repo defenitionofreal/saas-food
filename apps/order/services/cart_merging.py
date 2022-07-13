db93c64c759c7e242eb0f807b6c42ad642d46a8e
User <tgt@tgt.com>
2022-07-13 12:39:47 +0300

    implemented method

--- apps/order/services/cart_merging.py
+++ apps/order/services/cart_merging.py
@@ -4,3 +4,11 @@ def merge_two_carts(cart_to, cart_from):
     from apps.order.models import Cart
     if not isinstance(cart_to, Cart) or not isinstance(cart_from, Cart):
         return
+
+    for item in cart_from.items.all():
+        cart_to.add_or_merge_item_to_cart(item)
+
+    new_promo_code = cart_from.promo_code
+    if new_promo_code:
+        cart_to.promo_code = new_promo_code
+    cart_from.save()
