c50648b4501afa53d3bf61523bcfcc665dfaf2df
User <tgt@tgt.com>
2022-07-19 18:47:31 +0300

    product to dict helper method added

---
+++ apps/order/services/product_obj_to_cart_item_dict.py
@@ -0,0 +1,18 @@
+from apps.product.models import Product
+from ..models.cart_item import CartItemProductKeys as pkeys
+
+
+def get_cart_item_dict_from_product(product: Product, modifiers: dict = None, additives: list = None):
+    if product:
+        modifiers = modifiers if modifiers else {}
+        additives = additives if additives else []
+        return {
+            pkeys.id: product.id,
+            pkeys.category: product.category.slug,
+            pkeys.title: product.title,
+            pkeys.slug: product.slug,
+            pkeys.price: product.price,
+            pkeys.modifiers: modifiers,
+            pkeys.additives: additives
+        }
+    return {}
