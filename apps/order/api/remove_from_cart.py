afef40b654996dfe65e4323a65e06ce75a45b815
User <tgt@tgt.com>
2022-07-23 14:58:53 +0300

    remove from cart api refactored

--- apps/order/api/remove_from_cart.py
+++ apps/order/api/remove_from_cart.py
@@ -1,51 +1,22 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.generics import get_object_or_404
-
-from apps.product.models import Product
+from apps.order.services.cart_helper import CartHelper
 from apps.company.models import Institution
-from apps.order.models import Cart
 
-from django.conf import settings
-from django.db.models import F
+cart_item_id_key = 'cart_item_id'
 
 
 class RemoveFromCartAPIView(APIView):
-    """
-    Remove product from cart
-    - if auth look for a cart tied to a user
-    - if not auth look for a cart tied to a session cart id
-    - bottom logic looks for a product and rm it
-    """
+    """ Remove product from cart  """
 
-    def post(self, request, domain, product_slug):
+    def post(self, request, domain):
+        try:
+            cart_item_id = None
+            if cart_item_id_key in request.data:
+                cart_item_id = int(request.data[cart_item_id_key])
 
-        institution = Institution.objects.get(domain=domain)
-        product = get_object_or_404(Product, slug=product_slug)
-        user = self.request.user
-        session = self.request.session
-
-        if user.is_authenticated:
-            cart = Cart.objects.filter(institution=institution, customer=user)
-        else:
-            if not settings.CART_SESSION_ID in session:
-                return Response(
-                    {"detail": "Cart does not exist. (session cart)"})
-            cart = Cart.objects.filter(institution=institution,
-                                       session_id=session[settings.CART_SESSION_ID])
-
-        if cart.exists():
-            cart = cart[0]
-            if cart.items.filter(product__slug=product.slug).exists():
-                cart_item = cart.items.get(product__slug=product.slug,
-                                           cart=cart)
-                if cart_item.quantity > 1:
-                    cart_item.quantity = F("quantity") - 1
-                    cart_item.save(update_fields=("quantity",))
-                else:
-                    cart.items.remove(cart_item)
-                return Response({"detail": "Product quantity updated"})
-            else:
-                return Response({"detail": "This product not in a cart"})
-        else:
-            return Response({"detail": "Don't have a cart"})
+            institution = Institution.objects.get(domain=domain)
+            cart = CartHelper(request=request, institution=institution)
+            return cart.remove_product_or_decrease_quantity_by_id(cart_item_id=cart_item_id)
+        except ValueError:
+            return Response({"detail": "This product not in a cart"})
