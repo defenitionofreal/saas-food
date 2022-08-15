b5fe94befa92c70451eb5b60a07f011f75b73450
defenitionofreal <flavors@inbox.ru>
2022-08-15 22:22:05 +0300

    move  basic cart actions to class

--- apps/order/api/remove_from_cart.py
+++ apps/order/api/remove_from_cart.py
@@ -1,13 +1,6 @@
 from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework.generics import get_object_or_404
-
-from apps.product.models import Product
 from apps.company.models import Institution
-from apps.order.models import Cart
-
-from django.conf import settings
-from django.db.models import F
+from apps.order.services.cart_helper import CartHelper
 
 
 class RemoveFromCartAPIView(APIView):
@@ -18,34 +11,7 @@ class RemoveFromCartAPIView(APIView):
     - bottom logic looks for a product and rm it
     """
 
-    def post(self, request, domain, product_slug):
-
+    def post(self, request, domain, product_id):
         institution = Institution.objects.get(domain=domain)
-        product = get_object_or_404(Product, slug=product_slug)
-        user = self.request.user
-        session = self.request.session
-
-        if user.is_authenticated:
-            cart = Cart.objects.filter(institution=institution, customer=user)
-        else:
-            if not settings.CART_SESSION_ID in session:
-                return Response(
-                    {"detail": "Cart does not exist. (session cart)"})
-            cart = Cart.objects.filter(institution=institution,
-                                       session_id=session[settings.CART_SESSION_ID])
-
-        if cart.exists():
-            cart = cart[0]
-            if cart.items.filter(product__slug=product.slug).exists():
-                cart_item = cart.items.get(product__slug=product.slug,
-                                           cart=cart)
-                if cart_item.quantity > 1:
-                    cart_item.quantity = F("quantity") - 1
-                    cart_item.save(update_fields=("quantity",))
-                else:
-                    cart.items.remove(cart_item)
-                return Response({"detail": "Product quantity updated"})
-            else:
-                return Response({"detail": "This product not in a cart"})
-        else:
-            return Response({"detail": "Don't have a cart"})
+        cart = CartHelper(request, institution)
+        return cart.remove_item(product_id)
---
+++ apps/order/services/cart_helper.py
@@ -0,0 +1,164 @@
+# core
+from django.conf import settings
+from django.db.models import F
+# apps
+from apps.order.services.generate_cart_key import _generate_cart_key
+from apps.order.models import Cart, CartItem
+from apps.order.serializers import CartSerializer
+# rest framework
+from rest_framework.response import Response
+from rest_framework import status
+# other
+
+
+class CartHelper:
+    """
+    Main cart class with all needed funcs and counts
+    """
+
+    def __init__(self, request, institution):
+        self.request = request
+        self.user = request.user
+        self.session = request.session
+        self.institution = institution
+
+    # ======= BASIC METHODS =======
+    # todo: здесь базовые методы, которые так или иначе часто необходимы
+
+    def _check_or_generate_session_cart_id_key(self):
+        """ cart_id in sessions needed for all further requests """
+        if settings.CART_SESSION_ID not in self.session:
+            self.session[settings.CART_SESSION_ID] = _generate_cart_key()
+        self.session.modified = True
+
+    def _is_user_auth(self) -> bool:
+        """ check if user is authenticated or not """
+        if self.user.is_authenticated:
+            return True
+        return False
+
+    def _cart_min_amount(self) -> int:
+        """ cart minimum amount rule """
+        value = self.institution.min_cart_value.values_list("cost", flat=True)
+        if value:
+            return value[0]
+        return 0
+
+    def _cart_get_or_create(self) -> tuple:
+        """ get or create cart """
+        self._check_or_generate_session_cart_id_key()
+        if self._is_user_auth():
+            cart, cart_created = Cart.objects.get_or_create(
+                institution=self.institution,
+                customer=self.user,
+                session_id=self.session[settings.CART_SESSION_ID],
+                min_amount=self._cart_min_amount())
+        else:
+            cart, cart_created = Cart.objects.get_or_create(
+                institution=self.institution,
+                session_id=self.session[settings.CART_SESSION_ID],
+                min_amount=self._cart_min_amount())
+        return cart, cart_created
+
+    # ======= CONDITIONS & DEDUCTIONS =======
+    # todo: здесь написать методы, которые разгрузят модель Cart
+
+    def get_total_cart(self):
+        cart, _ = self._cart_get_or_create()
+        items = cart.items.all()
+        return sum(i.get_total_item_price for i in items)
+
+    # ======= ACTIONS =======
+    # todo: здесь методы действий покупателя
+
+    def add_item(self, product_dict) -> Response:
+        """ add new item to cart or update quantity of an item """
+        cart, cart_created = self._cart_get_or_create()
+        cart_item, cart_item_created = CartItem.objects.get_or_create(
+            product=product_dict, cart=cart)
+
+        if not cart_created:
+            if cart.items.filter(product=product_dict).exists():
+                cart_item.quantity = F("quantity") + 1
+                cart_item.save(update_fields=("quantity",))
+                return Response({"detail": "Product quantity updated"},
+                                status=status.HTTP_201_CREATED)
+            else:
+                cart.items.add(cart_item)
+                return Response({"detail": "New product added"},
+                                status=status.HTTP_201_CREATED)
+        else:
+            cart.items.add(cart_item)
+            return Response({"detail": "Cart created and product added"},
+                            status=status.HTTP_201_CREATED)
+
+    def remove_item(self, product_id) -> Response:
+        """ remove item from cart """
+        cart, cart_created = self._cart_get_or_create()
+
+        if cart.items.filter(id=product_id).exists():
+            cart_item = cart.items.get(id=product_id,
+                                       cart=cart)
+            if cart_item.quantity > 1:
+                cart_item.quantity = F("quantity") - 1
+                cart_item.save(update_fields=("quantity",))
+            else:
+                cart.items.remove(cart_item)
+            return Response({"detail": "Product quantity updated"})
+        else:
+            return Response({"detail": "This product not in a cart"})
+
+    def get_cart(self) -> Response:
+        """ cart detail """
+        if self._is_user_auth():
+            if settings.CART_SESSION_ID in self.session:
+                cart = Cart.objects.filter(
+                    institution=self.institution,
+                    session_id=self.session[settings.CART_SESSION_ID]).first()
+                if cart and not cart.customer:
+                    cart.customer = self.user
+                    cart.save()
+                # cart, cart_created = Cart.objects.get_or_create(
+                #     institution=institution, customer=user)
+                #
+                # for session_item in session_cart.items.all():
+                #     session_item.cart = cart
+                #     session_item.save()
+                #
+                #     cart_item_duplicates = cart.items.filter(product__slug=session_item.product["slug"])
+                #     if cart_item_duplicates.exists():
+                #         for i in cart_item_duplicates:
+                #             i.quantity = F("quantity") + session_item.quantity
+                #             i.save(update_fields=("quantity",))
+                #     else:
+                #         cart.items.add(session_item)
+                #         cart.save()
+                #
+                # if session_cart.promo_code:
+                #     cart.promo_code = session_cart.promo_code
+                #     cart.save()
+                #
+                # session_cart.delete()
+                # del session[settings.CART_SESSION_ID]
+                # #session.flush()
+                else:
+                    cart, cart_created = self._cart_get_or_create()
+            else:
+                self._check_or_generate_session_cart_id_key()
+                cart = Cart.objects.filter(institution=self.institution,
+                                           session_id=self.session[
+                                               settings.CART_SESSION_ID],
+                                           customer=self.user).first()
+                if not cart:
+                    return Response(
+                        {"detail": "Cart does not exist. (auth cart)"},
+                        status=status.HTTP_400_BAD_REQUEST)
+        else:
+            cart, cart_created = self._cart_get_or_create()
+
+        if cart.items.exists():
+            serializer = CartSerializer(cart,
+                                        context={"request": self.request})
+            return Response(serializer.data)
+        else:
+            return Response({"detail": "Cart is empty."})
