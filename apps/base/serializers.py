cbe01f94e10d3df5c7f5899e294374aa74e22a31
defenitionofreal <flavors@inbox.ru>
2023-05-13 23:29:53 +0700

    unverify contact data when update profile

--- apps/base/serializers.py
+++ apps/base/serializers.py
@@ -12,15 +12,36 @@ class UserSerializer(serializers.ModelSerializer):
         fields = (
             'id', 'username', 'first_name', 'middle_name', 'last_name',
             'email', 'phone', 'image', 'password', 'is_customer',
-            'is_organization'
+            'is_organization', 'is_email_verified', 'is_sms_verified'
         )
         extra_kwargs = {
             'password': {'write_only': True},
             # 'phone': {'required': False}
         }
 
+    def get_fields(self):
+        fields = super().get_fields()
+        # Remove password field during updates
+        if self.instance:
+            del fields['password']
+        return fields
+
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        validated_data.pop('password', None)
+        email = validated_data.get('email')
+        phone = validated_data.get('phone')
+
+        if email and email != instance.email:
+            instance.is_email_verified = False
+
+        if phone and phone != instance.phone:
+            instance.is_sms_verified = False
+
+        instance = super().update(instance, validated_data)
+        return instance
 
 
 class LoginObtainPairSerializer(TokenObtainPairSerializer):
